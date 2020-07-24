47a7db7f301e688d6dc828ef00404caf2d259227
Tom White <tom.e.white@gmail.com>
2020-07-24 17:14:37 +0100

    BGEN reader implementation using bgen_reader (#1)
    * BGEN reader implementation using bgen_reader
    
    * Use encode_array from sgkit.
    Make coverage 100%.
    Add GH Action to run test and build.
    
    * Add package metadata
    
    * Doc bi-allelic, diploid limitation.
    
    * Test a variety of chunk sizes
    
    * Check size of array in dosage calculation
    
    * Fail if not bi-allelic
    
    * Rework tests for bi-allelic examples with separate/missing sample IDs

---
+++ sgkit_bgen/__init__.py
@@ -0,0 +1,3 @@
+from .bgen_reader import read_bgen  # noqa: F401
+
+__all__ = ["read_bgen"]
---
+++ sgkit_bgen/tests/test_bgen_reader.py
@@ -0,0 +1,40 @@
+import numpy.testing as npt
+import pytest
+from sgkit_bgen import read_bgen
+
+
+@pytest.mark.parametrize("chunks", [(100, 200), (100, 500), (199, 500), "auto"])
+def test_read_bgen(shared_datadir, chunks):
+    path = shared_datadir / "example.bgen"
+    ds = read_bgen(path, chunks=chunks)
+
+    # check some of the data (in different chunks)
+    assert ds["call/dosage"].shape == (199, 500)
+    npt.assert_almost_equal(ds["call/dosage"].values[1][0], 1.987, decimal=3)
+    npt.assert_almost_equal(ds["call/dosage"].values[100][0], 0.160, decimal=3)
+
+
+def test_read_bgen_with_sample_file(shared_datadir):
+    # The example file was generated using
+    # qctool -g sgkit_bgen/tests/data/example.bgen -og sgkit_bgen/tests/data/example-separate-samples.bgen -os sgkit_bgen/tests/data/example-separate-samples.sample -incl-samples sgkit_bgen/tests/data/samples
+    # Then editing example-separate-samples.sample to change the sample IDs
+    path = shared_datadir / "example-separate-samples.bgen"
+    ds = read_bgen(path)
+    # Check the sample IDs are the ones from the .sample file
+    assert ds["sample/id"].values.tolist() == ["s1", "s2", "s3", "s4", "s5"]
+
+
+def test_read_bgen_with_no_samples(shared_datadir):
+    # The example file was generated using
+    # qctool -g sgkit_bgen/tests/data/example.bgen -og sgkit_bgen/tests/data/example-no-samples.bgen -os sgkit_bgen/tests/data/example-no-samples.sample -bgen-omit-sample-identifier-block -incl-samples sgkit_bgen/tests/data/samples
+    # Then deleting example-no-samples.sample
+    path = shared_datadir / "example-no-samples.bgen"
+    ds = read_bgen(path)
+    # Check the sample IDs are generated
+    assert ds["sample/id"].values.tolist() == [
+        "sample_0",
+        "sample_1",
+        "sample_2",
+        "sample_3",
+        "sample_4",
+    ]
